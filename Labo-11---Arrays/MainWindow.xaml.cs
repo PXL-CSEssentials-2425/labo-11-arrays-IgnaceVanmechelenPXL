020fdccf3b353c6f2d3b120e40e81dce607c32f4
IgnaceVanmechelenPXL <12402669@student.pxl.be>
2024-11-19 10:28:07 +0100

    Labo done

--- Labo-11---Arrays/MainWindow.xaml.cs
+++ Labo-11---Arrays/MainWindow.xaml.cs
@@ -16,9 +16,36 @@ namespace Labo_11___Arrays
     /// </summary>
     public partial class MainWindow : Window
     {
+        int[] numbers = new int[6] { 100, 50, 20, 60, 90, 80 };
+        int maximum = 0;
+        int minimum = int.MaxValue;
+        int[] smallestAndBiggestNumber = new int[2];
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void outputButton_Click(object sender, RoutedEventArgs e)
+        {
+            int[] resultaat = ReturnSmallestAndBiggest(numbers);
+            outputTextBox.Text = $"resultaat: {resultaat[0]}, {resultaat[1]}";
+        }
+        private int[] ReturnSmallestAndBiggest(int[] number)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > maximum)
+                {
+                    maximum = numbers[i];
+                }
+                if (numbers[i] < minimum)
+                {
+                    minimum = numbers[i];
+                }
+            }
+            int[] smallestAndBiggestNumber = new int[2] { minimum, maximum };
+            return smallestAndBiggestNumber;
         }
     }
 }
